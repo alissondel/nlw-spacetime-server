acc7a2a29d2c0846d26b6487a3f6bc60d2a22780
Alisson Delatim <alisson.delatim@hotmail.com>
2023-05-19 18:18:23 -0300

    .

---
+++ readme.md
@@ -0,0 +1,37 @@
+## Running aplication 
+
+```bash
+$ npm run dev
+$ npm run lint
+```
+
+## Install dependencies, devDependencies and Commands
+
+```bash
+$ npm init -y
+$ npm install typescript -D
+$ npm install @types/node -D
+$ npx tsc --init
+$ npm install tsx -D
+$ npm install fastify
+$ npm install eslint -D
+$ npm install @rocketseat/eslint-config -D
+$ npm install prisma -D
+$ npm install @prisma/client
+$ npm install zod
+$ npm install dotenv -D
+$ npm install axios
+$ npm install @fastify/cors
+$ npm install @fastify/jwt
+$ npm install @fastify/multipart
+$ npm install @fastify/static
+$ npm install @prisma/client
+```
+
+## Commands Prisma
+
+```bash
+$ npx prisma init --datasource-provider SQLite
+$ npx prisma migrate dev
+$ npx prisma studio
+```
---
+++ src/server.ts
@@ -0,0 +1,44 @@
+import 'dotenv/config'
+
+// FASTIFY
+import fastify from 'fastify'
+import cors from '@fastify/cors'
+import jwt from '@fastify/jwt'
+import multipart from '@fastify/multipart'
+
+// ROUTES
+import { memoriesRoutes } from './routes/memories'
+import { authRoutes } from './routes/auth'
+import { uploadRoutes } from './routes/upload'
+import { resolve } from 'path'
+
+const app = fastify()
+
+app.register(multipart)
+
+// Libera rota para visualizar a imagem
+app.register(require('@fastify/static'), {
+  root: resolve(__dirname, '../uploads'),
+  prefix: '/uploads',
+})
+
+app.register(cors, {
+  origin: true,
+})
+
+app.register(jwt, {
+  secret: process.env.JWT_SECRET as string,
+})
+
+app.register(authRoutes)
+app.register(uploadRoutes)
+app.register(memoriesRoutes)
+
+app
+  .listen({
+    port: 3333,
+    host: '0.0.0.0',
+  })
+  .then(() => {
+    console.log('🚀 HTTP server running on port http://localhost:3333')
+  })
